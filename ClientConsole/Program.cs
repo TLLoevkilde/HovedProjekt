e863e40d234ce118e53e9e1fcb45c030349a5586
Thomas <tlloevkilde@gmail.com>
2025-05-16 12:53:25 +0200

    Added basic ClientConsole

---
+++ ClientConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿var client = new HttpClient();
+
+Console.WriteLine("Requesting message from ResourceApi");
+var response = await client.GetAsync("https://localhost:7126/api/message");
+var content = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(content);
+Console.WriteLine("\nPress enter to exit...");
+Console.ReadLine();
