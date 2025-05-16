6b7b894e024dde495234621c823252b912cfe7ac
Thomas <tlloevkilde@gmail.com>
2025-05-16 12:37:44 +0200

    Add project files.

---
+++ ResourceApi/Controllers/MessageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResourceApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Hello from the Resource API!" });
+        }
+    }
+}
